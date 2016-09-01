57ea7f9018710e1e815d1d1b518f73c069dfec31
dariorios201612 <dariobancardrrcc@gmail.com>
2016-08-31 21:29:51 -0400

    version 2

--- src/main/java/py/edu/facitec/springtaller/dao/DAOGenerico.java
+++ src/main/java/py/edu/facitec/springtaller/dao/DAOGenerico.java
@@ -1,5 +1,30 @@
 package py.edu.facitec.springtaller.dao;
 
-public abstract class DAOGenerico {
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 
+public abstract class DAOGenerico<T> {
+	
+	
+	private Class<T> entityClass;
+	
+	public DAOGenerico(Class<T> entityClass) {
+		this.entityClass=entityClass;
+	}
+		protected abstract EntityManager getEntityManager();
+public void insertar(T entity){
+	getEntityManager().persist(entity);
+	
+}
+
+public void actualizar(T entity){
+	getEntityManager().merge(entity);
+}
+
+public void eliminar(T entity){
+	getEntityManager().remove(getEntityManager().contains(entity)?
+			entity:getEntityManager().merge(entity)
+			);
+	
+}
 }
